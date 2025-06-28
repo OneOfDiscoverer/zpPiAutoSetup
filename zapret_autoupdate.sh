0ad5f06ec6ec66e56fb9425d7ae336a1e328ff0a
oneofdiscoverer <oneofdiscoverer@yandex.ru>
2025-06-28 14:17:33 +0000

    add git stash before pulling

--- zapret_autoupdate.sh
+++ zapret_autoupdate.sh
@@ -82,6 +82,7 @@ update_zapret() {
         log_message "Updates found. Pulling changes..."
         
         # Pull updates
+	git stash
         git pull 2>&1 | while read line; do
             log_message "GIT: $line"
         done
